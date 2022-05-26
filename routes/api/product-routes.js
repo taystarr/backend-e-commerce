6a09a519bc4363d1582adaf44a93f86c389a690e
Tay Starr <tstarrroberts@gmail.com>
2022-05-26 17:47:06 -0400

    add delete routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -127,6 +127,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({ message: 'No product with this id found'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -55,7 +55,7 @@ router.put('/:id', (req, res) => {
   })
   .then(dbTagData => {
     if (!dbTagData[0]) {
-      res.status(404).json({ message: 'No category with this id found'});
+      res.status(404).json({ message: 'No tag with this id found'});
       return;
     }
     res.json(dbTagData);
@@ -69,6 +69,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag with this id found'});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
