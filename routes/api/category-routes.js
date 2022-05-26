722f2c9f82557949e744d4b5f0f09ae70139bf83
Tay Starr <tstarrroberts@gmail.com>
2022-05-26 16:26:39 -0400

    add put routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,6 +48,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData[0]) {
+      res.status(404).json({ message: 'No category with this id found'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
